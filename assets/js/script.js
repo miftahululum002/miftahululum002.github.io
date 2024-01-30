860057585ddb5385118e95736738305d1115504b
O027-MIFTAHUL ULUM,S.Pd <ulumiftahul06@gmail.com>
2024-01-30 13:04:45 +0700

    [FIX] content to json file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,48 @@ function getTemplate(index, data) {
   return template;
 }
 
-$(function () {
+function getContainerSkill(data) {
+  return `<div class="progress">
+	<span class="skill">${data.name} <i class="val">${data.value}${data.unit}</i></span>
+	<div class="progress-bar-wrap">
+		<div class="progress-bar" role="progressbar" aria-valuenow="${data.value}" aria-valuemin="0"
+			aria-valuemax="100">
+		</div>
+	</div>
+</div>`;
+}
+
+function getSkills() {
+  const fecthOptions = getFetchOptions();
+  const urlSkill = "assets/json/skills.json";
+  fetch(urlSkill, fecthOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      let dataParsing = data.skills;
+      dataParsing.forEach((element, index) => {
+        let row = getContainerSkill(element);
+        if (element.category == "teknis") {
+          $("#container-teknis").append(row);
+        } else {
+          $("#container-other").append(row);
+        }
+      });
+    });
+}
+
+function getFetchOptions() {
   let fecthOptions = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   };
+  return fecthOptions;
+}
+
+function getPortoTim() {
   const url = "assets/json/data.json";
+  const fecthOptions = getFetchOptions();
   fetch(url, fecthOptions)
     .then((response) => response.json())
     .then((data) => {
@@ -30,8 +64,11 @@ $(function () {
         $("#portfolio-body").append(row);
       });
     });
+}
 
+function getPortoPersonal() {
   const urlPersonal = "assets/json/datapersonal.json";
+  const fecthOptions = getFetchOptions();
   fetch(urlPersonal, fecthOptions)
     .then((response) => response.json())
     .then((data) => {
@@ -41,4 +78,10 @@ $(function () {
         $("#portfolio-personal-body").append(row);
       });
     });
+}
+
+$(function () {
+  getSkills();
+  getPortoTim();
+  getPortoPersonal();
 });
