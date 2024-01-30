7709407dc11cf7c0874d4eb8fa5786e9aff41a96
O027-MIFTAHUL ULUM,S.Pd <ulumiftahul06@gmail.com>
2024-01-30 10:56:14 +0700

    [FIX] protofolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,44 @@
-/** 
- * @author: O027-MIFTAHUL ULUM S.Pd 
- * @date: 2023-04-22 22:18:53 
- * @desc:  
+/**
+ * @author: O027-MIFTAHUL ULUM S.Pd
+ * @date: 2023-04-22 22:18:53
+ * @desc:
  */
 function getTemplate(index, data) {
-    let template = `<tr>
-        <td>${(index + 1)}</td>
+  let template = `<tr>
+        <td>${index + 1}.</td>
         <td>${data.name}</td>
         <td>${data.year}</td>
         <td>${data.link}</td>
     </tr>`;
-    return template;
+  return template;
 }
 
 $(function () {
-    let fecthOptions = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    }
-    const url = "assets/json/data.json";
-    fetch(url, fecthOptions).then((response) => response.json()).then((data) => {
-        let dataParsing = data.portfolios;
-        dataParsing.forEach((element, index) => {
-            let row = getTemplate(index, element);
-            $('#portfolio-body').append(row);
-        });
+  let fecthOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const url = "assets/json/data.json";
+  fetch(url, fecthOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      let dataParsing = data.portfolios;
+      dataParsing.forEach((element, index) => {
+        let row = getTemplate(index, element);
+        $("#portfolio-body").append(row);
+      });
     });
 
-    const urlPersonal = "assets/json/datapersonal.json";
-    fetch(urlPersonal, fecthOptions).then((response) => response.json()).then((data) => {
-        let dataParsing = data.portfolios;
-        dataParsing.forEach((element, index) => {
-            let row = getTemplate(index, element);
-            $('#portfolio-personal-body').append(row);
-        });
+  const urlPersonal = "assets/json/datapersonal.json";
+  fetch(urlPersonal, fecthOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      let dataParsing = data.portfolios;
+      dataParsing.forEach((element, index) => {
+        let row = getTemplate(index, element);
+        $("#portfolio-personal-body").append(row);
+      });
     });
-})
+});
