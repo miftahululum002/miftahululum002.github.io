cdb943948c906e608ee0fa2d19f8f548d077b2bb
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-12-24 08:58:30 +0700

    lang config

--- app/src/lang.tsx
+++ app/src/lang.tsx
@@ -3,8 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import en from './languages/en.json';
 import id from './languages/id.json';
 i18n.use(initReactI18next).init({
-    fallbackLng: 'id',
-    lng: 'id',
+    fallbackLng: localStorage.getItem('lang') || 'en',
+    lng: localStorage.getItem('lang') || 'en',
     resources: {
         en: {
             translations: en
@@ -18,5 +18,4 @@ i18n.use(initReactI18next).init({
 });
 
 i18n.languages = ['en', 'id'];
-
 export default i18n;
